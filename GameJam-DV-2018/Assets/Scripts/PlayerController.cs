e3ab0e60a016fb5b7a56ea37084ed0eff1cb7213
Alexis <asessarego@quadiontech.com>
2018-09-07 23:54:49 -0300

    Player movement and camera following

--- GameJam-DV-2018/Assets/Scripts/PlayerController.cs
+++ GameJam-DV-2018/Assets/Scripts/PlayerController.cs
@@ -2,23 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
     Rigidbody2D rig;
+    Vector3 _origPos;
 
+    public float speed = 50f;
     private void Awake()
     {
         rig = this.GetComponent<Rigidbody2D>();
+        //Set original position
+        _origPos = gameObject.transform.position;
+
     }
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Obtain direction vector
+        Vector3 moveDirection = gameObject.transform.position - _origPos;
+
+        if (moveDirection != Vector3.zero)
+        {
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+        //Obtain directional movement defined on the inputs
         float verticalSpeed = Input.GetAxis("Vertical");
         float horizontalSpeed = Input.GetAxis("Horizontal");
-        rig.velocity = new Vector2(horizontalSpeed, verticalSpeed);
+        //Update speed based on the directional movement and the defined movement speed
+        rig.velocity = new Vector2(horizontalSpeed, verticalSpeed) * speed;
+        //Update original position
+        _origPos = gameObject.transform.position;
     }
 }
