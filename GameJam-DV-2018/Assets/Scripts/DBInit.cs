13b4223674e473c36ee749361418b002a342107d
eramis <ezequiel.ramis@gmail.com>
2018-09-08 10:31:08 -0300

    Added High Score service

---
+++ GameJam-DV-2018/Assets/Scripts/DBInit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DBInit : MonoBehaviour {
+
+    private string dbPath;
+
+    // Use this for initialization
+    void Awake ()
+    {
+        dbPath = "URI=file:" + Application.persistentDataPath + "/database.db";
+
+        HighScoreService highScoreService = new HighScoreService(dbPath);
+        highScoreService.CreateTable();
+    }
+
+}
