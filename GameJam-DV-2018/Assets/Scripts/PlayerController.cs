e0ec6d7d05dd0f624b911d19dfad6ac1e479d94e
Alexis <asessarego@quadiontech.com>
2018-09-07 21:05:29 -0300

    base game
    sprite moving

---
+++ GameJam-DV-2018/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    Rigidbody2D rig;
+
+    private void Awake()
+    {
+        rig = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float verticalSpeed = Input.GetAxis("Vertical");
+        float horizontalSpeed = Input.GetAxis("Horizontal");
+        rig.velocity = new Vector2(horizontalSpeed, verticalSpeed);
+    }
+}
