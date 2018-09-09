a249032f9f76fd121bb53b791dac2465d8df56c2
Alexis <asessarego@quadiontech.com>
2018-09-09 02:48:37 -0300

    Life spawn logic

---
+++ GameJam-DV-2018/Assets/Scripts/Controllers/LifeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+            if (player.lives < 3)
+            {
+                player.lives++;
+                player.updateLife();
+            }
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
